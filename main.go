d4f4d53f60485a6d1426e87bb02f2b9830f5cec0
kitfloppa <timofeev.anik@students.dvfu.ru>
2022-07-09 23:56:08 +0300

    task 4 core done

--- main.go
+++ main.go
@@ -2,77 +2,146 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
-	"os"
-	"path/filepath"
-	"strings"
+	"net/http"
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
-type Task struct {
-	duration time.Duration
-	number   int
+var (
+	count count32
+	q     Queue
+)
+
+type count32 int32
+
+func (c *count32) inc() int32 {
+	return atomic.AddInt32((*int32)(c), 1)
 }
 
-func Execute(t *Task, ch chan byte, wg *sync.WaitGroup) {
+func (c *count32) get() int32 {
+	return atomic.LoadInt32((*int32)(c))
+}
+
+type Task struct {
+	duration time.Duration
+	number   int32
+	resChan  chan byte
+}
+
+func Execute(t *Task) {
 	log.Printf("Starting task №%d...", t.number)
 	time.Sleep(t.duration)
 	log.Printf("Task №%d done!", t.number)
 
-	<-ch
-	wg.Done()
+	if t.resChan != nil {
+		<-t.resChan
+	}
+}
+
+type Queue struct {
+	sync.RWMutex
+	items []Task
+}
+
+func (q *Queue) Push(item Task) {
+	q.Lock()
+	defer q.Unlock()
+	q.items = append(q.items, item)
+}
+
+func (q *Queue) Pop() Task {
+	q.Lock()
+	defer q.Unlock()
+	if len(q.items) == 0 {
+		return Task{}
+	}
+	item := q.items[0]
+	q.items = q.items[1:]
+	return item
+}
+
+func (q *Queue) GetAll() []Task {
+	q.RLock()
+	defer q.RUnlock()
+	return q.items
+}
+
+func TaskLoop(q *Queue, terminator <-chan byte) {
+	for {
+		select {
+		case <-terminator:
+			return
+		default:
+			task := q.Pop()
+			if task.number == 0 {
+				time.Sleep(time.Millisecond) // waiting 1s for new tasks
+				continue
+			}
+			Execute(&task)
+		}
+	}
+
+}
+
+func add(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != "POST" {
+		http.Error(w, "Method is not supported.", http.StatusNotFound)
+		return
+	}
+
+	keys := r.URL.Query()["timeDuration"]
+	if keys == nil {
+		http.Error(w, "Require timeDuration parameter", http.StatusNotFound)
+		return
+	}
+
+	tds := keys[0]
+
+	var waiting bool
+
+	if flag := r.URL.Query()["sync"]; flag != nil {
+		waiting = true
+	} else if flag := r.URL.Query()["async"]; flag != nil {
+		waiting = false
+	} else {
+		http.Error(w, "Require flag sync/async", http.StatusNotFound)
+		return
+	}
+
+	td, err := time.ParseDuration(tds)
+	if err != nil {
+		http.Error(w, "Require timeDuration parameter", http.StatusNotFound)
+		return
+	}
+
+	num := count.get()
+	count.inc()
+	if waiting {
+		wait := make(chan byte, 1)
+		wait <- 1
+		q.Push(Task{duration: td, number: num, resChan: wait})
+		wait <- 1
+	} else {
+		q.Push(Task{duration: td, number: num, resChan: nil})
+	}
+
+	fmt.Fprintf(w, "Succes")
 }
 
 func main() {
+	count.inc()
 
-	args := os.Args
+	term := make(chan byte)
 
-	if len(args) != 2 {
-		fmt.Printf("Usage: ./%s [input-file]\n", filepath.Base(args[0]))
-		os.Exit(-1)
+	go TaskLoop(&q, term)
+
+	http.HandleFunc("/add", add)
+
+	log.Printf("Starting http server ...\n")
+	if err := http.ListenAndServe(":8081", nil); err != nil {
+		log.Fatal(err)
 	}
-
-	fmt.Println("Input count of processors:")
-
-	var maxProcessorCount int
-	_, err := fmt.Scan(&maxProcessorCount)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	timefile, err := ioutil.ReadFile(args[1])
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	lines := strings.Split(string(timefile), "\n")
-
-	var tasks []Task
-
-	for i, str := range lines {
-		str = strings.Trim(str, "\r") // For windows OS
-		t, err := time.ParseDuration(str)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-		i := i
-		tasks = append(tasks, Task{duration: t, number: i})
-	}
-
-	ch := make(chan byte, maxProcessorCount)
-
-	var wg sync.WaitGroup
-
-	for i := 0; i < len(tasks); i++ {
-		wg.Add(1)
-		ch <- 1
-		go Execute(&tasks[i], ch, &wg)
-	}
-
-	wg.Wait()
 }
