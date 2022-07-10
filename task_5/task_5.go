b1d7a05497287d4a554dee05acac61258c56b8cf
kitfloppa <timofeev.anik@students.dvfu.ru>
2022-07-10 12:50:14 +0300

    task 5 draft

--- task_5/task_5.go
+++ task_5/task_5.go
@@ -1,1 +1,20 @@
-package task_5
+package main
+
+import (
+	"github.com/golang/protobuf/proto"
+	"os"
+)
+
+func main() {
+	cit := &Cities{}
+
+	f, _ := os.Open("./pb/example1.pb")
+
+	bz := make([]byte, 1024)
+	_, _ = f.Read(bz)
+
+	proto.Unmarshal(bz, cit)
+
+	i := 0
+	_ = i
+}
