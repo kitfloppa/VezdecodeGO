9cadb594875031b5fb5b5c8e5c3055c10fba92eb
kitfloppa <timofeev.anik@students.dvfu.ru>
2022-07-10 11:47:11 +0300

    add stress testing

--- main_test.go
+++ main_test.go
@@ -8,6 +8,7 @@ import (
 	"net/url"
 	"sync"
 	"testing"
+	"time"
 )
 
 func TestServerOneThreadAsync(t *testing.T) {
@@ -18,6 +19,7 @@ func TestServerOneThreadAsync(t *testing.T) {
 	term := make(chan byte)
 
 	go New(term, httpServerExitDone)
+	time.Sleep(time.Millisecond) // sleep for server starting
 
 	data := url.Values{
 		"timeDuration": {"0h0m10s"},
@@ -59,6 +61,7 @@ func TestServerOneThreadSync(t *testing.T) {
 	term := make(chan byte)
 
 	go New(term, httpServerExitDone)
+	time.Sleep(time.Millisecond) // sleep for server starting
 
 	data := url.Values{
 		"timeDuration": {"0h0m10s"},
@@ -85,6 +88,60 @@ func TestServerOneThreadSync(t *testing.T) {
 	}
 
 	require.Equal(t, "0s", string(body))
+
+	term <- 1
+
+	httpServerExitDone.Wait()
+}
+
+func TestServerStressAsync(t *testing.T) {
+
+	httpServerExitDone := &sync.WaitGroup{}
+
+	httpServerExitDone.Add(1)
+
+	term := make(chan byte)
+
+	go New(term, httpServerExitDone)
+	time.Sleep(time.Millisecond) // sleep for server starting
+
+	data := url.Values{
+		"timeDuration": {"0h0m10s"},
+		"async":        {""},
+	}
+
+	f := func(res chan<- byte) {
+		for i := 0; i < 10; i++ {
+			_, err := http.PostForm("http://localhost:8081/add", data)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+		res <- 1
+	}
+
+	results := make(chan byte, 100)
+	for j := 0; j < 50; j++ {
+		go f(results)
+	}
+
+	for j := 0; j < 50; j++ {
+		<-results
+	}
+
+	resp, err := http.Get("http://localhost:8081/time")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	require.Equal(t, "1h23m10s", string(body))
 
 	term <- 1
 
